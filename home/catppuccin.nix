6d09931e01a559ebdf9f2d1cf17ac291b3af6fd2
tinker <tinker@null.computer>
2024-10-25 13:44:23 +0300

    tmux theme setup

--- home/catppuccin.nix
+++ home/catppuccin.nix
@@ -6,5 +6,9 @@
   catppuccin = {
     enable = true;
     flavor = "mocha";
+    accent = "mauve"; # “blue”, “flamingo”, “green”, “lavender”, “maroon”, “mauve”, “peach”, “pink”, “red”, “rosewater”, “sapphire”, “sky”, “teal”, “yellow”
+    pointerCursor.enable = false; # not supported for darwin
+    pointerCursor.accent = "mauve"; # “blue”, “flamingo”, “green”, “lavender”, “maroon”, “mauve”, “peach”, “pink”, “red”, “rosewater”, “sapphire”, “sky”, “teal”, “yellow”
+
   };
 }
