6935d140248bae57f8c2817ab1428abad14e6b7f
tinker <tinker@null.computer>
2024-10-27 08:42:33 +0200

    change catppuccin theme accent to lavender

--- home/catppuccin.nix
+++ home/catppuccin.nix
@@ -6,7 +6,7 @@
   catppuccin = {
     enable = true;
     flavor = "mocha";
-    accent = "mauve"; # “blue”, “flamingo”, “green”, “lavender”, “maroon”, “mauve”, “peach”, “pink”, “red”, “rosewater”, “sapphire”, “sky”, “teal”, “yellow”
+    accent = "lavender"; # “blue”, “flamingo”, “green”, “lavender”, “maroon”, “mauve”, “peach”, “pink”, “red”, “rosewater”, “sapphire”, “sky”, “teal”, “yellow”
     pointerCursor.enable = false; # not supported for darwin
     pointerCursor.accent = "mauve"; # “blue”, “flamingo”, “green”, “lavender”, “maroon”, “mauve”, “peach”, “pink”, “red”, “rosewater”, “sapphire”, “sky”, “teal”, “yellow”
   };
