0564146642fb7c186f239b648297f6095a431d37
tinker <tinker@null.computer>
2024-11-07 16:48:32 +0200

    remove kitty opacity

--- home/base/gui/kitty.nix
+++ home/base/gui/kitty.nix
@@ -15,7 +15,7 @@
       enable_audio_bell = false;
       mouse_hide_wait = "-1.0";
       window_padding_width = 10;
-      background_opacity = "0.8";
+      background_opacity = "1";
       background_blur = 5;
       cursor_shape = "underline"; # block, beam, underline
       disable_ligatures = "never";
