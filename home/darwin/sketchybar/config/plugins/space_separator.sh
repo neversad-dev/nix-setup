42affc951cfcb65fd06b1bff1225250d440f4dd0
tinker <tinker@null.computer>
2024-11-17 20:35:26 +0200

    sketchybar: show aerospace mode

---
+++ home/darwin/sketchybar/config/plugins/space_separator.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ "$SENDER" = "aerospace_mode_change" ]; then
+
+  if [ -n "$AEROSPACE_MODE" ]; then
+    sketchybar --set $NAME icon="[$AEROSPACE_MODE]" \
+      --set $NAME icon.drawing=on
+  else
+    sketchybar --set $NAME icon.drawing=off
+  fi
+
+fi
