63f30986e900f125239c0778adee318e18bd0f6b
tinker <tinker@null.computer>
2025-01-11 19:50:16 +0200

    remove unused config

--- modules/base.nix
+++ modules/base.nix
@@ -15,41 +15,8 @@
   environment.systemPackages = with pkgs; [
     git # used by nix flakes
 
-    # archives
-    zip
-    xz
-    zstd
-    unzipNLS
-    p7zip
+    just # a command runner like make, but simpler
 
-    # Text Processing
-    # Docs: https://github.com/learnbyexample/Command-line-text-processing
-    gnugrep # GNU grep, provides `grep`/`egrep`/`fgrep`
-    gnused # GNU sed, very powerful(mainly for replacing text in files)
-    gawk # GNU awk, a pattern scanning and processing language
-    jq # A lightweight and flexible command-line JSON processor
-
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    wget
-    curl
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc # it is a calculator for the IPv4/v6 addresses
-
-    # misc
-    file
-    findutils
-    which
-    tree
-    gnutar
-    rsync
-    alejandra # The Uncompromising Nix Code Formatter
-    shfmt # A shell parser and formatter
   ];
 
   users.users.${myvars.username} = {
